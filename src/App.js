d94e296cdd200c14f24aa05da0bf0b3bcf52780e
kiana <kianakermani2001@gmail.com>
2024-03-03 22:38:00 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <a href="https://github.com/kianakermani" className="btn btn-primary">
+        hello
+      </a>
+    </div>
+  );
+}
