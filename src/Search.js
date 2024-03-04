c578b81ba1039947f8fa3bcffb29c7813e0c910c
kiana <kianakermani2001@gmail.com>
2024-03-04 18:40:59 +0330

    added api

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+function handleResponse(r) {
+  console.log(r.data[0]);
+}
 export default function Search() {
   let [word, setWord] = useState("");
   function handle(e) {
     e.preventDefault();
-    alert(`Searching for ${word} definition`);
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(url).then(handleResponse);
   }
+
   function update(e) {
     setWord(e.target.value);
   }
   return (
     <div>
       <form onSubmit={handle}>
-        <input type="Search" autoFocus="true" onChange={update} />
+        <input type="Search" autoFocus={true} onChange={update} />
       </form>
     </div>
   );
